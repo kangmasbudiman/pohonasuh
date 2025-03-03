4fc3cffb07d246acb58bbeadbb70676d97757b2b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-03-03 08:14:38 +0000

    updateToIntenetChek

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import '/custom_code/actions/index.dart' as actions;
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GoRouter.optionURLReflectsImperativeAPIs = true;
   usePathUrlStrategy();
+
+  // Start initial custom actions code
+  await actions.connected();
+  // End initial custom actions code
 
   await SupaFlow.initialize();
 
